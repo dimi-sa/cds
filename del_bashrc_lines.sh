7daa0dd9dd6ab8bf3bbe6c5c7eca745ca2461707
DS <saridakisbitos@gmail.com>
2024-10-12 13:53:47 +0300

    regular cd on unknown stuff

--- del_bashrc_lines.sh
+++ del_bashrc_lines.sh
@@ -4,5 +4,5 @@ grep_out=$(grep -n $1 $2)
 if [ $? ]
 then
   line_num=$(echo $grep_out | cut -d ":" -f 1)
-	sed -i $(( $line_num-1 )),"$line_num"d $2
+  sed -i $(( $line_num-1 )),"$line_num"d $2
 fi
